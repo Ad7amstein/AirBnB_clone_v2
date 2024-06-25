fcde570d5d5f2e4c1947076e0a8f8e846055bcc2
Ad7amstein <adham32003200@gmail.com>
2024-06-25 14:47:55 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Creat a new user if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on 'hbnb_dev_db'
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on 'performance_schema'
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
