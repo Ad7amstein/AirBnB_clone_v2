309c83ecb2ec65779fcc49cae5ab428a85131584
Ad7amstein <adham32003200@gmail.com>
2024-06-28 22:36:43 -0700

    add db storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""DataBase Storage"""
+from sqlalchemy import create_engine, text
+import os
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+environ = os.environ
+class DBStorage:
+    """Represents the database storage"""
+    __engine = None
+    __session =  None
+
+    def __init__(self):
+        """Initialization method"""
+        user, password, host_name, db_name = environ['HBNB_MYSQL_USER'],\
+                environ['HBNB_MYSQL_PWD'], environ['HBNB_MYSQL_HOST'],\
+                environ['HBNB_MYSQL_DB']
+        self.__engine = create_engine(
+                "mysql+mysqldb://{}:{}@{}/{}".format(
+                    user, password, host_name, db_name), pool_pre_ping=True)
+        if environ['HBNB_ENV'] == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query all objects"""
+        classes = {
+                    'State': State, 'City': City,
+                    }
+        objects = {}
+        if cls is None:
+            for class_name in classes:
+                instances = self.__session.query(classes[class_name]).all()
+                print(f"INSTANCES: {instances}")
+                for instance in instances:
+                    key = class_name + '.' + instance.id
+                    objects[key] = instance
+        else:
+            if cls.__name__ in classes:
+                instances = self.__session.query(classes[cls.__name__]).all()
+                for instance in instances:
+                    key = cls.__name__ + '.' + instance.id
+                    objects[key] = instance
+        return objects
+    
+    def new(self, obj):
+        """Add new object"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Save all changes"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes an object"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reloads the database"""
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session = scoped_session(Session)
+        self.__session = session()
