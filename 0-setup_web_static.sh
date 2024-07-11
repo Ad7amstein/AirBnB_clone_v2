4ba3093fa72ad0be0b61868f0881bf01e6f817ab
Ad7amstein <adham32003200@gmail.com>
2024-07-11 07:36:48 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
 # Prepare the web server
 
-# Function to check for errors
-check_error() {
-  if [ $? -ne 0 ]; then
-    echo "Error: $1"
-    exit 1
-  fi
-}
-
-# Install Nginx
-sudo apt-get update
-check_error "Failed to update package index."
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-check_error "Failed to install nginx."
-sudo systemctl start nginx
-check_error "Failed to start nginx."
-sudo systemctl enable nginx
-check_error "Failed to enable nginx."
-
-# Create some directories if not exist
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-check_error "Failed to create directories."
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
 
 # Create a fake HTML file for testing
 echo "<html>
@@ -31,19 +14,9 @@ echo "<html>
     Web static test
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-check_error "Failed to create fake HTML file."
 
-# Create a symlink
-SYMLINK="/data/web_static/current"
-TARGET="/data/web_static/releases/test"
-
-# Remove the existing symbolic link if it exists and create a new one
-sudo ln -sfn "$TARGET" "$SYMLINK"
-check_error "Failed to create symbolic link."
-
-# Give ownership to ubuntu
-sudo chown -R ubuntu:ubuntu /data/
-check_error "Failed to set ownership."
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 
 # Update nginx config
 sudo sh -c ' echo "
